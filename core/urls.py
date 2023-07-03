4e0de8447252a6c9e6a4bdf50dfb104d971ae6ff
willy <will.castro@duocuc.cl>
2023-07-03 00:17:56 -0400

    firstcommit

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,11 @@ from django.contrib.auth.views import LoginView
 from django.urls import path
 from .views import *
 
+
 urlpatterns = [
-    path('', home),
+    path('', home, name="home"),
     path('login', LoginView.as_view(template_name='core/login.html'), name="login"),
+    path('registrar', registrar, name="registrar"),
+    path('about', about, name="about"),
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
 from django.contrib.auth.views import logout_then_login
-# Create your views here.
+
+
 
 def home(request):
     return render(request, 'core/index.html ')
 
 def login(request):
     return render(request, 'core/login.html')
+
+
+def registrar(request):
+    return render(request, 'core/registrar.html')
+
+def about(request):
+    return render(request, 'core/about.html')
 
 
